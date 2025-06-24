ee9aacc898aa02aa446f804ee1a34e2a3ff8b750
JoDaeChan <daechan476@gmail.com>
2025-06-24 15:24:01 +0900

    update project page and enable store-automation demo link

--- app/page.tsx
+++ app/page.tsx
@@ -395,7 +395,7 @@ export default function Portfolio() {
 							image="/store-img.png?height=400&width=600"
 							demoUrl="/projects/store-automation"
 							repoUrl="https://github.com/daechan-jo/store-automation"
-							disabled={true}
+							disabled={false}
 						/>
 						<ProjectCard
 							title="Spring WebFlux MSA Architecture"
